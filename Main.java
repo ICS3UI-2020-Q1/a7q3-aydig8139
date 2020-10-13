26468061aa455cc8367fdd52bf0a38ec3b521c61
aydig8139 <aydig8139@wrdsb.ca>
2020-10-13 18:05:50 +0000

    unfinished question

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 /**
  *
  * @author 
  */
 public class Main {
+
+  public static void chaotic(int randNum){
+    int randNum = (int)(Math.random() * (5 - 1 + 1)) + 1;
+  }
 
   /**
    * The method that is executed when you hit the run button.
@@ -10,6 +16,17 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner(System.in);
     
+    System.out.println("enter number");
+    int randNum = input.nextInt;
+
+    while (randNum <= 5){
+      System.out.println("*");
+    }
+
+    chaotic(randNum);
+
+
   }
 }
