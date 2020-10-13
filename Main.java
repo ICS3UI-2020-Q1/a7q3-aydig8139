52709658a9aaac20428acef2ccc7fed0e97d2b6c
aydig8139 <aydig8139@wrdsb.ca>
2020-10-13 18:36:48 +0000

    finished random asterisk question

--- Main.java
+++ Main.java
@@ -6,8 +6,22 @@ import java.util.Scanner;
  */
 public class Main {
 
-  public static void chaotic(int randNum){
-    int randNum = (int)(Math.random() * (5 - 1 + 1)) + 1;
+  //create method to generate random numbers and print asterisks
+  public static void chaotic(int numOfLines){
+
+    //create a for loop to repeat and generate random number of asterisks depending on the input number of lines by the user
+    for(int i = 1; i <= numOfLines; i++){
+
+      //formula to generate random numbers
+      int randNum = (int)(Math.random() * (5 - 1 + 1)) + 1;
+
+       //for loop to print random number of asterisks on a line
+       for(int j = 1; j <= randNum; j++){
+         System.out.print("*");
+       }
+       System.out.println("*");
+    }
+    
   }
 
   /**
@@ -15,18 +29,14 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
     Scanner input = new Scanner(System.in);
     
-    System.out.println("enter number");
-    int randNum = input.nextInt;
+    //ask user for the number of lines they would like to generate random asterisks and store it in a variable mentioned in the above parameter
+    System.out.println("Enter the number of lines you would like to generate random asterisks");
+    int numOfLines = input.nextInt();
 
-    while (randNum <= 5){
-      System.out.println("*");
-    }
-
-    chaotic(randNum);
-
+    chaotic(numOfLines);
 
   }
 }
